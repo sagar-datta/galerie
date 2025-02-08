5d3350777bebb57f6722902cde18eb5897889520
sagar-datta <s@gar.jp.net>
2025-02-08 20:16:38 +0800

    refactor: Update layout structure in MainFooter and App components for improved responsiveness

--- src/components/MainFooter.tsx
+++ src/components/MainFooter.tsx
@@ -2,16 +2,29 @@ import { COLORS } from "../constants/colors";
 
 export function MainFooter() {
   return (
-    <div
-      className="flex items-center px-8 overflow-hidden"
-      style={{ backgroundColor: COLORS.coral }}
-    >
-      <span
-        className="text-6xl font-bold tracking-widest"
-        style={{ color: COLORS.beige }}
+    <div className="w-full h-full grid grid-rows-4 overflow-hidden">
+      <div
+        className="overflow-hidden flex items-center px-8"
+        style={{ backgroundColor: COLORS.beige }}
       >
-        hey
-      </span>
+        <span
+          className="text-6xl font-bold tracking-widest truncate"
+          style={{ color: COLORS.red }}
+        >
+          hey
+        </span>
+      </div>
+      <div
+        className="overflow-hidden flex items-center px-8 row-span-3"
+        style={{ backgroundColor: COLORS.coral }}
+      >
+        <span
+          className="text-6xl font-bold tracking-widest truncate"
+          style={{ color: COLORS.beige }}
+        >
+          hey
+        </span>
+      </div>
     </div>
   );
 }
