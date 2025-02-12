2d9d56946b92f8c92c6fbbd7d16a51a23c6f81c6
sagar-datta <s@gar.jp.net>
2025-02-12 18:21:27 +0800

    feat: Add GitHub Pages deployment workflow and set base URL in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/galerie/", // Base URL for GitHub Pages
   plugins: [react(), tailwindcss()],
   build: {
     // Enable minification
