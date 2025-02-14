d2cf7f249493084f0773e3df4390e85723132790
sagar-datta <s@gar.jp.net>
2025-02-14 19:47:14 +0800

    feat: Add new images and metadata to Barcelona gallery

--- src/lib/data/galleries/barcelona.ts
+++ src/lib/data/galleries/barcelona.ts
@@ -4,6 +4,414 @@ export const barcelona: CityGallery = {
   city: "Barcelona",
   images: [
   {
+    "id": "d8b38bb3c09b082e73485451b98954f7",
+    "publicId": "at1fr579qax4nuwmrsug",
+    "width": 5669,
+    "height": 7559,
+    "metadata": {
+      "dateTaken": "2024:12:27 13:44:49",
+      "model": "iPhone 14 Pro Max",
+      "gpsLatitude": "41 deg 23' 30.53\" N",
+      "gpsLongitude": "2 deg 9' 54.54\" E",
+      "exposureTime": "1/277",
+      "aperture": "1.8",
+      "focalLength": "24 mm",
+      "iso": "80",
+      "lensModel": "iPhone 14 Pro Max back triple camera 6.86mm f/1.78"
+    }
+  },
+  {
+    "id": "6ffb0263e9b24529bc603804fef4eda7",
+    "publicId": "vjjxs2jvp7sksmmo1jun",
+    "width": 5371,
+    "height": 9704,
+    "metadata": {
+      "dateTaken": "2025:01:27 12:19:33",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 24' 12.96\" N",
+      "gpsLongitude": "2 deg 10' 27.59\" E",
+      "exposureTime": "1/60",
+      "aperture": "4.0",
+      "focalLength": "30 mm",
+      "iso": "1100",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
+    "id": "21860e4f62a483aa654895316e883d89",
+    "publicId": "plbjddnhoiwn3nfo5k0m",
+    "width": 3712,
+    "height": 5568,
+    "metadata": {
+      "dateTaken": "2025:01:27 11:17:54",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 24' 12.96\" N",
+      "gpsLongitude": "2 deg 10' 27.59\" E",
+      "exposureTime": "1/80",
+      "aperture": "4.0",
+      "focalLength": "39 mm",
+      "iso": "2500",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
+    "id": "757ca80bdd8b5d3157082423b70456ed",
+    "publicId": "inmdknuctmts0h4aqzn3",
+    "width": 3712,
+    "height": 5568,
+    "metadata": {
+      "dateTaken": "2025:01:27 10:53:03",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 24' 12.96\" N",
+      "gpsLongitude": "2 deg 10' 27.59\" E",
+      "exposureTime": "1/60",
+      "aperture": "4.0",
+      "focalLength": "30 mm",
+      "iso": "1600",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
+    "id": "f5f2d3a0b1d81f3dcf9bac1caf30af1b",
+    "publicId": "wp67mm9nk61h2rfskooj",
+    "width": 6048,
+    "height": 8064,
+    "metadata": {
+      "dateTaken": "2024:12:26 09:14:10",
+      "model": "iPhone 14 Pro Max",
+      "gpsLatitude": "41 deg 23' 27.77\" N",
+      "gpsLongitude": "2 deg 10' 50.34\" E",
+      "exposureTime": "1/651",
+      "aperture": "1.8",
+      "focalLength": "24 mm",
+      "iso": "80",
+      "lensModel": "iPhone 14 Pro Max back triple camera 6.86mm f/1.78"
+    }
+  },
+  {
+    "id": "ec8a386e231a5b35cde4dd73050327fa",
+    "publicId": "zndjfmviniaprcsidmb6",
+    "width": 3495,
+    "height": 5242,
+    "metadata": {
+      "dateTaken": "2025:01:27 12:12:42",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 24' 12.96\" N",
+      "gpsLongitude": "2 deg 10' 27.59\" E",
+      "exposureTime": "1/100",
+      "aperture": "8.0",
+      "focalLength": "45 mm",
+      "iso": "2500",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
+    "id": "d9a8fae62ac7b19ecfeadbaa8dd1b8cd",
+    "publicId": "xj1p6yno7ajmhctydps1",
+    "width": 3712,
+    "height": 5568,
+    "metadata": {
+      "dateTaken": "2025:01:27 12:19:09",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 24' 12.96\" N",
+      "gpsLongitude": "2 deg 10' 27.59\" E",
+      "exposureTime": "1/60",
+      "aperture": "4.0",
+      "focalLength": "30 mm",
+      "iso": "720",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
+    "id": "a3e6d786e332abeb223b655e21f45569",
+    "publicId": "jbcltrbtv7jca1ciekfi",
+    "width": 3024,
+    "height": 4032,
+    "metadata": {
+      "dateTaken": "2024:12:27 13:45:06",
+      "model": "iPhone 14 Pro Max",
+      "gpsLatitude": "41 deg 23' 30.78\" N",
+      "gpsLongitude": "2 deg 9' 54.84\" E",
+      "exposureTime": "1/156",
+      "aperture": "2.2",
+      "focalLength": "14 mm",
+      "iso": "40",
+      "lensModel": "iPhone 14 Pro Max back triple camera 2.22mm f/2.2"
+    }
+  },
+  {
+    "id": "e258ddc9241509b62b6b29d55b1c8b2d",
+    "publicId": "oai3njhrmcxnck5ltm3d",
+    "width": 5201,
+    "height": 6931,
+    "metadata": {
+      "dateTaken": "2024:12:27 13:40:48",
+      "model": "iPhone 14 Pro Max",
+      "gpsLatitude": "41 deg 23' 30.34\" N",
+      "gpsLongitude": "2 deg 9' 54.73\" E",
+      "exposureTime": "1/100",
+      "aperture": "1.8",
+      "focalLength": "24 mm",
+      "iso": "80",
+      "lensModel": "iPhone 14 Pro Max back triple camera 6.86mm f/1.78"
+    }
+  },
+  {
+    "id": "bdb7d20a9e5fcb9a68420dcc5ff67a18",
+    "publicId": "no4a331fzow8cxrc5q3z",
+    "width": 3712,
+    "height": 5568,
+    "metadata": {
+      "dateTaken": "2025:01:27 11:28:47",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 24' 12.96\" N",
+      "gpsLongitude": "2 deg 10' 27.59\" E",
+      "exposureTime": "1/125",
+      "aperture": "4.2",
+      "focalLength": "57 mm",
+      "iso": "2200",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
+    "id": "a859f1cda90a9b3f28d0e03177166bc4",
+    "publicId": "ly1cwg42juxfoejktrod",
+    "width": 3020,
+    "height": 6191,
+    "metadata": {
+      "dateTaken": "2025:01:27 12:13:37",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 24' 12.96\" N",
+      "gpsLongitude": "2 deg 10' 27.59\" E",
+      "exposureTime": "1/60",
+      "aperture": "8.0",
+      "focalLength": "30 mm",
+      "iso": "640",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
+    "id": "784d8081b840492f78d786bbe9a9ddaf",
+    "publicId": "ph51ibstuys3ko3vjybp",
+    "width": 5568,
+    "height": 3712,
+    "metadata": {
+      "dateTaken": "2024:12:28 16:51:38",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 22' 6.90\" N",
+      "gpsLongitude": "2 deg 9' 12.54\" E",
+      "exposureTime": "1/80",
+      "aperture": "4.0",
+      "focalLength": "27 mm",
+      "iso": "400",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
+    "id": "a9aebfa33918ebbf0f832a0a34dbf99b",
+    "publicId": "hhjeu7wasm6eurd3tadk",
+    "width": 3712,
+    "height": 4451,
+    "metadata": {
+      "dateTaken": "2024:12:29 15:02:00",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 22' 55.84\" N",
+      "gpsLongitude": "2 deg 10' 26.81\" E",
+      "exposureTime": "1/1250",
+      "aperture": "4.8",
+      "focalLength": "84 mm",
+      "iso": "400",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
+    "id": "fea6e450425cfd204e7480168db4f9f6",
+    "publicId": "dpljwmnxsiyu7i99jyru",
+    "width": 2296,
+    "height": 3923,
+    "metadata": {
+      "dateTaken": "2024:12:25 22:33:42",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 24' 12.96\" N",
+      "gpsLongitude": "2 deg 10' 27.59\" E",
+      "exposureTime": "1/1000",
+      "aperture": "5.6",
+      "focalLength": "210 mm",
+      "iso": "100",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
+    "id": "102286b70d11c687e440eb68c714e770",
+    "publicId": "edqfjkzzc4yvym9lt937",
+    "width": 5568,
+    "height": 3712,
+    "metadata": {
+      "dateTaken": "2024:12:28 12:21:20",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 22' 6.85\" N",
+      "gpsLongitude": "2 deg 9' 36.32\" E",
+      "exposureTime": "1/5000",
+      "aperture": "4.0",
+      "focalLength": "40 mm",
+      "iso": "100",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
+    "id": "42bf88213d0aa6d5750f35ee5319986e",
+    "publicId": "hnvjrxfziu5atpsutmpk",
+    "width": 3468,
+    "height": 5201,
+    "metadata": {
+      "dateTaken": "2024:12:30 07:55:51",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 24' 15.89\" N",
+      "gpsLongitude": "2 deg 10' 31.98\" E",
+      "exposureTime": "1/80",
+      "aperture": "4.0",
+      "focalLength": "36 mm",
+      "iso": "1250",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
+    "id": "ac55aaad81f6012a51d85b85faba86ec",
+    "publicId": "avbc5zd7szv8jhhxy0n6",
+    "width": 3712,
+    "height": 5568,
+    "metadata": {
+      "dateTaken": "2024:12:29 14:32:05",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 23' 1.67\" N",
+      "gpsLongitude": "2 deg 10' 55.19\" E",
+      "exposureTime": "1/2000",
+      "aperture": "4.0",
+      "focalLength": "45 mm",
+      "iso": "400",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
+    "id": "5a0ca05047d24e0f5f8f32d86a9a79b6",
+    "publicId": "x6doznu1kykbsvchkjkl",
+    "width": 3612,
+    "height": 5418,
+    "metadata": {
+      "dateTaken": "2025:01:27 10:50:01",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 24' 12.96\" N",
+      "gpsLongitude": "2 deg 10' 27.59\" E",
+      "exposureTime": "1/125",
+      "aperture": "4.2",
+      "focalLength": "57 mm",
+      "iso": "560",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
+    "id": "323ede5598c338375e27fe7af5ac1fc3",
+    "publicId": "apnzr0ezfabsuscudkp9",
+    "width": 3712,
+    "height": 5568,
+    "metadata": {
+      "dateTaken": "2024:12:29 14:37:02",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 22' 55.84\" N",
+      "gpsLongitude": "2 deg 10' 26.81\" E",
+      "exposureTime": "1/3200",
+      "aperture": "4.0",
+      "focalLength": "45 mm",
+      "iso": "400",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
+    "id": "3cd3af2d5f55c173c06094325353bf9c",
+    "publicId": "al5fbevvjroic2d00swe",
+    "width": 3433,
+    "height": 5149,
+    "metadata": {
+      "dateTaken": "2024:12:25 22:35:28",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 24' 12.96\" N",
+      "gpsLongitude": "2 deg 10' 27.59\" E",
+      "exposureTime": "1/1250",
+      "aperture": "5.0",
+      "focalLength": "93 mm",
+      "iso": "100",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
+    "id": "0bc06520b699244132b12d39c54df6e2",
+    "publicId": "ypr8kxvofdje9uvwgmbd",
+    "width": 3712,
+    "height": 5568,
+    "metadata": {
+      "dateTaken": "2024:12:28 14:02:49",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 22' 6.85\" N",
+      "gpsLongitude": "2 deg 9' 36.32\" E",
+      "exposureTime": "1/3200",
+      "aperture": "4.0",
+      "focalLength": "45 mm",
+      "iso": "100",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
+    "id": "8028e1c147d3c25cae22e2d5d28d5d2e",
+    "publicId": "vaotxrepik00tf5hwuth",
+    "width": 3712,
+    "height": 5568,
+    "metadata": {
+      "dateTaken": "2024:12:28 16:51:05",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 22' 6.90\" N",
+      "gpsLongitude": "2 deg 9' 12.54\" E",
+      "exposureTime": "1/500",
+      "aperture": "5.0",
+      "focalLength": "93 mm",
+      "iso": "400",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
+    "id": "ba97e894e6b5d2a1a8c67850712f25fd",
+    "publicId": "w4gaudxfj7rtqr64rskf",
+    "width": 3712,
+    "height": 5568,
+    "metadata": {
+      "dateTaken": "2024:12:27 16:28:25",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 23' 43.34\" N",
+      "gpsLongitude": "2 deg 9' 42.89\" E",
+      "exposureTime": "1/80",
+      "aperture": "4.0",
+      "focalLength": "34 mm",
+      "iso": "400",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
+    "id": "93a83e679300cc62e1c2b8228878316b",
+    "publicId": "lyzncicpyakb1xswogh5",
+    "width": 3712,
+    "height": 5568,
+    "metadata": {
+      "dateTaken": "2024:12:27 17:28:52",
+      "model": "NIKON D7500",
+      "gpsLatitude": "41 deg 23' 43.34\" N",
+      "gpsLongitude": "2 deg 9' 42.89\" E",
+      "exposureTime": "1/80",
+      "aperture": "4.0",
+      "focalLength": "36 mm",
+      "iso": "160",
+      "lensModel": "Nikon AF-S DX Nikkor 18-140mm f/3.5-5.6G ED VR"
+    }
+  },
+  {
     "id": "daa43a9e887d12f3f9e0396d6f727670",
     "publicId": "qw8hy60suhtymj9evokf",
     "width": 3712,
